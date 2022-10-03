3f697ccd4d30437ca6ad39d41afb445fb0e49e29
crawlive <crawfolivia@gmail.com>
2022-10-02 21:21:16 -0500

    mess splitting out functions compare needs to be fixxed out of bounds exception prob

--- cs4773-hw2/src/main/java/com/war/WarVersions.java
+++ cs4773-hw2/src/main/java/com/war/WarVersions.java
@@ -3,26 +3,88 @@ package com.war;
 import com.Player.Player;
 import com.card.Cards;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 
 public class WarVersions {
 
+    static ArrayList<Player> winners = new ArrayList<>();
+    static Player winner = null;
     public static EndCases endCase = null;
+    private static ArrayList<Cards> deckWon = new ArrayList<>();
+    static ArrayList<Player> players = new ArrayList<>();
 
+    static ArrayList<Player> warPlayers = new ArrayList<>();
+    static ArrayList<Cards> warCards = new ArrayList<>();
+
+    static List<Integer> deckSizes = new ArrayList<>();
+
+    private static void resetRound(){
+        winner = null;
+        deckWon.clear();
+        warCards.clear();
+        warPlayers.clear();
+    }
     //TODO: MOVE CARDS INITIALIZATION HERE MAYBE
     public static void warOne (int maxRounds, Player playerOne, Player playerTwo) {
         int currentRound = 0;
-        while(currentRound < maxRounds && playerOne.getDeckSize() > 0 && playerTwo.getDeckSize() > 0){
-            Cards onesCard = playerOne.flipCards();
-            Cards twosCard = playerTwo.flipCards();
-            //printCardsPulled(onesCard, twosCard);
+        players = new ArrayList<>(Arrays.asList(playerOne, playerTwo));
+        while(currentRound < maxRounds && !emptyDeckCheck()){
+            resetRound();
+            warCards = pullCards(players);
+            printCardsPulled(warCards);
+            deckWon.addAll(warCards);
+            //set winner
+            //winner = compareCards();
 
-            ArrayList<Cards> deckWon = new ArrayList<>(Arrays.asList(onesCard, twosCard));
-            if(onesCard.rank < twosCard.rank){              //player 2 won
+            //winner.won(deckWon);
+            if(warCards.get(0).rank < warCards.get(1).rank){              //player 2 won
+                playerTwo.won(deckWon);
+            }
+            else if(warCards.get(0).rank > warCards.get(1).rank){         //player 1 won
+                playerOne.won(deckWon);
+            }
+            else {
+                warPlayers.addAll(players);
+                war();
+            }
+            if(endCase != null)
+                break;
+            currentRound++;
+        }
+        if(endCase == null){
+            if(currentRound == maxRounds){
+                endCase = EndCases.ROUNDS_END;
+            }
+            else if(playerOne.getDeckSize() == 0 || playerOne.getDeckSize() == 0 ) {
+                endCase = EndCases.ONE_WINNER;
+            }
+        }
+
+        deckSizes.add(playerOne.getPoints());
+        deckSizes.add(playerTwo.getPoints());
+        System.out.println(playerOne.deck.toString());
+        System.out.println(playerTwo.deck.toString());
+        findWinner();
+        endGame();
+
+    }
+
+    private static boolean emptyDeckCheck(){
+        for(Player player : players){
+            if(player.getDeckSize() == 0) {
+                endCase = EndCases.ONE_WINNER;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static Player compareCards() {
+        /*if(onesCard.rank < twosCard.rank){              //player 2 won
                 playerTwo.won(deckWon);
             }
             else if(onesCard.rank > twosCard.rank){         //player 1 won
@@ -30,77 +92,93 @@ public class WarVersions {
             }
             else {
                 war(deckWon, new ArrayList<>(Arrays.asList(playerOne, playerTwo)));
-            }
-            if(endCase != null)
-                break;
-            currentRound++;
-        }
-        if(currentRound == maxRounds){
-            endCase = EndCases.ROUNDS_END;
-        }
-        List<Integer> deckSizes = new ArrayList<>(Arrays.asList(playerOne.getPoints(), playerTwo.getPoints()));
-        System.out.println(playerOne.deck.toString());
-        System.out.println(playerTwo.deck.toString());
-        endGame(deckSizes);
+            }*/
 
+        Player winner = null;
+        ArrayList<Integer> ranks = new ArrayList<>();
+        for(Cards card: warCards){
+            ranks.add(card.rank);
+        }
+        int highestCard = Collections.max(ranks);
+        int frequency = Collections.frequency(ranks, highestCard);
+        if(frequency == 1){
+            int cardOwner = ranks.indexOf(highestCard);
+            ranks.remove(cardOwner);
+            winner = players.get(cardOwner);
+        } else {
+            for (int i = 0; i < ranks.size(); i++) {
+                int cardRank = ranks.get(i);
+                if (cardRank == highestCard) {
+                    warPlayers.add(players.get(i));
+                }
+            }
+            war();
+        }
+        return winner;
+
+        /*
+        int highestRank = -1;
+        for(int i = 0; i < warCards.size(); i++){
+            if(warCards.get(i).rank > highestRank){
+                winner = players.get(i);
+            } else if (warCards.get(i).rank == highestRank) {
+                warPlayers.add(winner);
+                warPlayers.add(players.get(i));
+            }
+        }
+        if(warPlayers.size() >= 2)
+            war();*/
+        /*if(onesCard.rank < twosCard.rank){              //player 2 won
+            playerTwo.won(deckWon);
+        }
+        else if(onesCard.rank > twosCard.rank){         //player 1 won
+            playerOne.won(deckWon);
+        }
+        else {
+            war(deckWon, new ArrayList<>(Arrays.asList(playerOne, playerTwo)));
+        }*/
     }
 
     //TODO: adjust for war v3
-    private static void endGame(List<Integer> deckSizes) {
-        int winner;
+    private static void endGame() {
         switch(endCase){
             case ONE_WINNER: //one player has won all the cards
             case RAN_OUT:
-                //winner = findWinner(deckSizes);
-                findWinner(deckSizes);   //TODO DOES NOT WORK NEED TO MAKE DYNAMIC ON PLAYER AND ON # WINNERS
-                //System.out.println("Player " + winner + " has won all of the cards");
+                printWinner();
                 break;
             case ROUNDS_END:
                 System.out.println("Max number of rounds played");
-                findWinner(deckSizes); //TODO DOES NOT WORK NEED TO MAKE DYNAMIC ON PLAYER AND ON # WINNERS
+                printWinner();
                 break;
             case DRAW_CARDS:
                 System.out.println("Draw: Players last cards were equal cards");
+                printWinner();
                 break;
             default:
                 System.out.println("Some error occurred.");
         }
     }
 
-    private static void findWinner(List<Integer> deckSizes) {
-         /*int winner = 0, biggestDeck = 0;
-        for (int i = 0; i < deckSizes.size(); i++) {
-            int player = i+1;
-            if (biggestDeck < deckSizes.get(i)) {
-                biggestDeck = deckSizes.get(i);
-                winner = player;
-            }
-            else if(biggestDeck == deckSizes.get(i)){
-                winner.add(player);
+    private static void printWinner() {
+        if(winners.size() == 1) {
+            System.out.print("Player " + winners.get(0).playerNum + " has won the game with ");
+            System.out.print(winners.get(0).getPoints() + " cards");
+        } else {
+            for (Player player : winners) {
+                System.out.print("Player " + player.playerNum + " has won the game with ");
+                System.out.print(player.getPoints());
             }
         }
-       ArrayList<Integer> winners = new ArrayList<>();
-        ArrayList<Integer> scores = new ArrayList<>();
-        int biggestDeck = 0;
-        for (int i = 0; i < deckSizes.size(); i++) {
-            int player = i+1;
-            if (biggestDeck < deckSizes.get(i)) {
-                biggestDeck = deckSizes.get(i);
-                winners.add(player);
-            }
-            else if(biggestDeck == deckSizes.get(i)){
-                winners.add(player);
-            }
-        }
-        if(winners.size() > 1){
-            System.out.print("Tie: Players ");
-            winners.forEach((n) -> { System.out.print(n + ", "); });
-            System.out.println(" have deck sizes of ");
-        }
-        else{
-            System.out.println("Player " + winners.get(0) + " has won the most cards");
-        }
-        return winner;*/
+    }
+
+    private static void findWinner() {
+        int biggestDeck = Collections.max(deckSizes);
+        int indexOfDeck = deckSizes.indexOf(biggestDeck);
+        deckSizes.remove(indexOfDeck);
+        winner = players.get(indexOfDeck);
+        winners.add(winner);
+        if(deckSizes.contains(biggestDeck))
+            findWinner();
     }
 
     /*Most descriptions of War are not clear about what happens if a player runs out of cards
@@ -119,11 +197,12 @@ card) while player A plays a card face down and one face up, which is a nine. Pl
 war and takes all these seven cards (the five cards that A played and the two cards that B
 played) and the game continues normally.*/
     //check decks before proceeding with iterations
-    private static void war(ArrayList<Cards> deckWon, ArrayList<Player> players) {
+    //todo change to only do war for players who have war
+    private static void war() {
         System.out.println("*** WAR! ***");
-        for(int i = 0; i < players.size(); i++){
-            warCheck(players);
-            warIteration(deckWon, players);
+        //for(int i = 0; i < players.size(); i++){
+            warCheck();
+            warIteration();
             //nested check loop maybe
             /* if(checkDecks(playerOne) && checkDecks(playerTwo)){    //both are fine
                 System.out.println("Players have enough cards for war");
@@ -143,42 +222,58 @@ played) and the game continues normally.*/
                 giveCardsBack(deckWon, playerOne, playerTwo);
                 endCase = EndCases.DRAW_CARDS;
             }*/
-        }
+        //}
     }
 
-
-    private static void warCheck(ArrayList<Player> players) {
-        boolean check = true, draw = false;
-        for (Player player : players) {
+//Todo if one player is out of cards make sure game doesnt end if other 2 players have cards still
+    private static void warCheck() {
+        int ablePlayers = warPlayers.size();
+        boolean check = true;
+        boolean draw = false;
+        for (Player player : warPlayers) {
             if (!checkDecks(player)) {
                 System.out.println("Player " + player.playerNum + " does not have enough cards to play war!");
-                if (!check)
-                    draw = true;
-                else
-                    check = false;
+                deckWon.addAll(player.deck);
+                player.deck.clear();
+                if (!check)             //if check is equal to false, ive set it before
+                    draw = true;        //there is a draw
+                check = false;
+                ablePlayers--;          //to handle when 3+ players
             }
         }
-        if(draw)
+        if(draw) {
+            giveCardsBack();
             endCase = EndCases.DRAW_CARDS;
-        else if (!draw && !check)
+        }
+        else if (!check && ablePlayers <= 1) {
             endCase = EndCases.RAN_OUT;
+        }
     }
 
-
+/*
     private static void giveCards(ArrayList<Cards> deckWon, Player winner, Player loser) {
-        deckWon.addAll(winner.deck);
-        deckWon.addAll(loser.deck);
+        deckWon.addAll(p1.deck);
+        deckWon.addAll(p2.deck);
         winner.deck.clear();
         loser.deck.clear();
         winner.won(deckWon);
-    }
+    }*/
 
-    private static void giveCardsBack(ArrayList<Cards> deckWon, Player playerOne, Player playerTwo) {
-        for(int i = 0; i < deckWon.size(); i++){
-            playerOne.addCard(deckWon.get(i));
-            playerTwo.addCard(deckWon.get(i+1));
-            i++;    //increment past second card
+    private static void giveCardsBack() {
+        int currPlayer = 0;
+        for (Cards card : deckWon) {
+            if (currPlayer > (warPlayers.size()-1))
+                currPlayer = 0;
+            warPlayers.get(currPlayer).addCard(card);
+            currPlayer++;
+
         }
+        deckWon.clear();
+        warPlayers.forEach((player) -> {
+            player.won(player.deck);
+            player.deck.clear();
+        });
+
     }
 
     //check deck function
@@ -187,9 +282,9 @@ played) and the game continues normally.*/
     }
 
     //begin war iterations
-    private static void warIteration(ArrayList<Cards> deckWon, ArrayList<Player> players) {
-        ArrayList<Cards> faceDownCards = pullCards(players);
-        ArrayList<Cards> warCards = pullCards(players);
+    private static void warIteration() {
+        ArrayList<Cards> faceDownCards = pullCards(warPlayers);
+        ArrayList<Cards> warCards = pullCards(warPlayers);
 
         deckWon.addAll(faceDownCards);
         deckWon.addAll(warCards);
@@ -197,15 +292,18 @@ played) and the game continues normally.*/
         printCardsPulled(warCards);
 
         //determineCard
-        if (onesWarCard.rank < twosWarCard.rank) {              //player 2 won
+       //winner = compareCards();
+       //winner.won(deckWon);
+        if (warCards.get(0).rank < warCards.get(1).rank) {              //player 2 won
             //player two wins add all 6 cards to points pile
-            playerTwo.won(deckWon);
-            playerOne.lost();
-        } else if (onesWarCard.rank > twosWarCard.rank) {         //player 1 won
-            playerOne.won(deckWon);
-            playerTwo.lost();
+            warPlayers.get(1).won(deckWon);
+            warPlayers.get(0).lost();
+
+        } else if (warCards.get(0).rank > warCards.get(1).rank) {         //player 1 won
+            warPlayers.get(0).won(deckWon);
+            warPlayers.get(1).lost();
         } else { //equal again, start war over
-            war(deckWon, playerOne, playerTwo);
+            war();
         }
     }
 
@@ -220,7 +318,7 @@ played) and the game continues normally.*/
 
     private static void printCardsPulled(ArrayList<Cards> cards) {
         for (int i =0; i < cards.size(); i++) {
-            System.out.println("Player" + i + " " + cards.get(i).toString());
+            System.out.println("Player " + (i+1) + " " + cards.get(i).toString());
         }
     }
 
@@ -229,9 +327,40 @@ played) and the game continues normally.*/
     // The winner is the player with the most cards in his/her points pile at the end of the game.
     // If the players do not have enough cards to complete a war, that round is a draw, and they keep their cards.
     public static void warTwo (Player playerOne, Player playerTwo) {
-        while(playerOne.getDeckSize() > 0 && playerTwo.getDeckSize() > 0){
-            Cards onesCard = playerOne.flipCards();
-            Cards twosCard = playerTwo.flipCards();
+        players = new ArrayList<>(Arrays.asList(playerOne, playerTwo));
+        while(!emptyDeckCheck()){
+            resetRound();
+            warCards = pullCards(players);
+            printCardsPulled(warCards);
+            deckWon.addAll(warCards);
+
+            if(warCards.get(0).rank < warCards.get(1).rank){              //player 2 won
+                playerTwo.won(deckWon);
+                playerOne.lost();
+            }
+            else if(warCards.get(0).rank > warCards.get(1).rank){         //player 1 won
+                playerOne.won(deckWon);
+                playerTwo.lost();
+            }
+            else {
+                System.out.println(warCards.get(0).rank + " is equal to " + warCards.get(1).rank);
+                warPlayers.addAll(players);
+                war();
+            }
+
+            /*winner = compareCards();
+
+            winner.won(deckWon);
+            if(winner.playerNum == playerOne.playerNum)
+                playerTwo.lost();
+            else
+                playerOne.lost();
+
+            if(endCase != null)
+                break;
+            */
+            /*Cards onesCard = playerOne.flipCards();
+            //Cards twosCard = playerTwo.flipCards();
             //ArrayList<Cards> cards = new ArrayList<>(Arrays.asList());
             //printCardsPulled(cards);
 
@@ -246,13 +375,22 @@ played) and the game continues normally.*/
             }
             else {
                 System.out.println(onesCard.rank + " is equal to " + twosCard.rank);
-                war(deckWon, playerOne, playerTwo);
-            }
-            if(endCase != null)
-                break;
+                war();
+            }*/
+
         }
-        List<Integer> deckSizes = new ArrayList<>(Arrays.asList(playerOne.getPoints(), playerTwo.getPoints()));
-        endGame(deckSizes);
+
+        if(endCase == null){
+           if(playerOne.getDeckSize() == 0 || playerOne.getDeckSize() == 0) {
+                endCase = EndCases.ONE_WINNER;
+            }
+        }
+
+        //List<Integer> deckSizes = new ArrayList<>(Arrays.asList(playerOne.getPoints(), playerTwo.getPoints()));
+        deckSizes.add(playerOne.getPoints());
+        deckSizes.add(playerTwo.getPoints());
+        findWinner();
+        endGame();
 
         System.out.println(playerOne.deck.toString());
         System.out.println(playerTwo.deck.toString());
@@ -265,6 +403,8 @@ played) and the game continues normally.*/
 
     public static void warThree (ArrayList<Player> players) {
         while(players.get(0).getDeckSize() > 0 && players.get(1).getDeckSize() > 0 && players.get(2).getDeckSize() > 0){
+            resetRound();
+            deckWon.clear();
             Cards onesCard = players.get(0).flipCards();
             Cards twosCard = players.get(1).flipCards();
             Cards threesCard = players.get(2).flipCards();
@@ -293,8 +433,11 @@ played) and the game continues normally.*/
                 break;
         }
 
-        List<Integer> deckSizes = new ArrayList<>(Arrays.asList(players.get(0).getPoints(), players.get(1).getPoints(), players.get(2).getDeckSize()));
-        endGame(deckSizes);
+        //List<Integer> deckSizes = new ArrayList<>(Arrays.asList(players.get(0).getPoints(), players.get(1).getPoints(), players.get(2).getDeckSize()));
+        deckSizes.add(players.get(0).getPoints());
+        deckSizes.add(players.get(1).getPoints());
+        deckSizes.add(players.get(2).getPoints());
+        endGame();
 
         System.out.println(players.get(0).deck.toString());
         System.out.println(players.get(1).deck.toString());
